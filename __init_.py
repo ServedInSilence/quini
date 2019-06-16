58909f0b272f8390bb4036c94708672c96530fde
ServedInSilence <48741841+ServedInSilence@users.noreply.github.com>
2019-06-16 13:58:17 -0700

    Create __init_.py

---
+++ __init_.py
@@ -0,0 +1,18 @@
+from .core import (
+    BYTE,
+    KIBIBYTE, KILOBYTE,
+    MEBIBYTE, MEGABYTE,
+    GIBIBYTE, GIGABYTE,
+    TEBIBYTE, TERABYTE,
+    PEBIBYTE, PETABYTE,
+    EXBIBYTE, EXABYTE,
+    ZEBIBYTE, ZETTABYTE,
+    YOBIBYTE, YOTTABYTE,
+  	QUINIBYTE, QUINIBYTE,
+    BinaryUnits, DecimalUnits, convert_units
+)
+
+__version__ = '1.0.1'
+#Algie Bookshleves All rights reseverd to the Author (C) 2008-2019
+
+
